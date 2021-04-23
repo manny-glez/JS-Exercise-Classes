5db45f49e2cac02d57c60ad04db7acf0c0f9a250
Emmanuel Gonzalez <dev.egonzalez1@gmail.com>
2021-04-22 23:56:45 -0500

    Answered Questions

--- index.js
+++ index.js
@@ -30,10 +30,10 @@ class Airplane {
   
   /*
     TASK 1
-      - Write a Person class whose constructor initializes `name` and `age` from arguments.
-      - All instances of Person should also initialize with an empty `stomach` array.
-      - Give instances of Person the ability to `.eat("someFood")`:
-          + When eating an edible, it should be pushed into the `stomach`.
+      - Write a Person class whose constructor initializes `name` and `age` from arguments. ✅
+      - All instances of Person should also initialize with an empty `stomach` array. ✅
+      - Give instances of Person the ability to `.eat("someFood")`: ✅
+          + When eating an edible, it should be pushed into the `stomach`. ✅
           + The `eat` method should have no effect if there are 10 items in the `stomach`.
       - Give instances of Person the ability to `.poop()`:
           + When an instance poops, its `stomach` should empty.
@@ -42,7 +42,27 @@ class Airplane {
   */
   
  class Person {
-    
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+      this.stomach = [];
+    }
+    eat(food) {
+
+    if(this.stomach.length < 10) {
+
+      this.stomach.push(food);
+      return 
+    }
+
+    }
+
+    poop() {
+      this.stomach = [];
+    }
+    toString() {
+      return `${this.name}, ${this.age}`;
+    }
   }
   
   /*
@@ -60,8 +80,26 @@ class Airplane {
   */
   
  class Car {
-    
+    constructor(model, milesPerGallon) {
+      this.model = model;
+      this.milesPerGallon = milesPerGallon;
+      this.tank = 0;
+      this.odometer = 0;
+    }
+    fill(gallons) {
+      this.tank = this.tank + gallons;
+    }
+    drive(distance) {
+      this.odometer = this.odometer + distance;
+
+      this.tank = this.tank - (distance / this.milesPerGallon);
+      
+      if(this.tank === 0) {
+        return `I ran out of fuel at ${this.odometer} miles!`;
+      }
+    }
   }
+
   
   /*
     TASK 3
@@ -76,25 +114,53 @@ class Airplane {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
-    
+    constructor(attributes) {
+      this.name = attributes.name;
+      this.age = attributes.age;
+      this.location = attributes.location;
+    }
+     
+    speak() {
+      return `Hello my name is ${this.name}, I am from ${this.location}`;
+    }
   }
-  
+
   /*
     TASK 4
       - Write an Instructor class extending Lambdasian.
       - Its constructor takes a single argument - an object with the following keys:
-          + All the keys used to initialize instances of Lambdasian.
+          + All the keys used to initialize instances of Lambdasian. ✅
           + `specialty`: what the instance of Instructor is good at, i.e. 'redux'
           + `favLanguage`: i.e. 'JavaScript, Python, Elm etc.'
           + `catchPhrase`: i.e. `Don't forget the homies`.
       - The constructor calls the parent constructor passing it what it needs.
       - The constructor should also initialize `specialty`, `favLanguage` and `catchPhrase` properties on the instance.
       - Instructor instances have the following methods:
-          + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
+          + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in. ✅
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+ class Instructor extends Lambdasian {
 
+  constructor(attributes) {
+
+    // super calls the properties to be inherited
+    super(attributes.name, attributes.age, attributes.location);
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  }
+   
+  demo(subject) {
+      return `Today we are learning about ${subject}`
+    }
+
+  grade(student, subject) {
+    return `${student.name} receives a perfect score on ${subject}`
+  }
  }
   /*
     TASK 5
@@ -111,8 +177,29 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
-     
+ class Student extends Lambdasian {
+    constructor(attributes) {
+
+      super(attributes.name, attributes.age, attributes.location);
+      this.name = attributes.name;
+      this.age = attributes.age;
+      this.location = attributes.location;
+
+      this.previousBackground = attributes.previousBackground;
+      this.className = attributes.className;
+      this.favSubjects = attributes.favSubjects;
+    }
+    listSubjects() {
+      return `Loving ${this.favSubjects}!`
+    }
+
+    PRAssignment(subject) {
+      return `${this.name} has submitted a PR for ${subject}`
+    }
+
+    sprintChallenge(subject) {
+      return `${this.name} has begun sprint challenge on ${subject}`
+    }
  }
   
   /*
@@ -128,8 +215,33 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor {
+   constructor(attributes) {
+
+    super(attributes.name, attributes.age, attributes.location, attributes.specialty, attributes.favLanguage, attributes.catchPhrase);
+
+    // inherited from the Person class
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+
+    // inherited from the Instructor class
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+
+    // Unique attributes on PM class
+    this.gradClassName = attributes.gradClassName;
+    this.favInstructor = attributes.favInstructor;
+    }
+
+    standUp() {
+
+    }
+
+    DebugsCode() {
+      
+    }
  }
   /*
     STRETCH PROBLEM (no tests!)
